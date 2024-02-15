67844072eb459b4317dc0e92379479085812da05
Nirajan singh <72257324+Ni2221@users.noreply.github.com>
2024-02-15 18:06:50 +0530

    fourth  commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args)  {
         System.out.println("Jai Shree ram");
         System.out.println("kaise hai app log");
+        System.out.println("welcome to NirajanDevHub");
     }
 }
